8ff69d318cac730101a78945d1ed33f02b358d52
karim <karih.dev@gmail.com>
2022-10-11 00:19:24 +0300

    - relations added - policies added

---
+++ app/Filament/Resources/DepartmentResource/RelationManagers/StocksRelationManager.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Resources\DepartmentResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\CheckboxColumn;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StocksRelationManager extends RelationManager
+{
+    protected static string $relationship = 'stocks';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required(),
+                TextInput::make('slug')->required(),
+                Checkbox::make('is_active'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->sortable(),
+                TextColumn::make('slug')->sortable(),
+                CheckboxColumn::make('is_active')
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+}
